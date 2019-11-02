8cd23bfb405120268e3875be607d949055377336
Yasmin Hillis <jasminehillis94@gmail.com>
2019-11-02 22:08:55 +0200

    adds the selection-sort code

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let selectionSort = (arr) => {
   for (i=0; i<arr.length-1; i++){
-
+    for (j=i+1; j<arr.length; j++){
+      if(arr[j]<arr[i]){
+        let temp = arr[i];
+          console.log(arr[i]);
+          arr[i] = arr[j];
+         arr[j] = temp;
+      }
+    }
   }
   return arr;
 }
